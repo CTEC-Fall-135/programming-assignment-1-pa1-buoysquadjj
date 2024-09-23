ad0c5672147bf130f8a871fbf10ba0fac0b5ac25
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2024-09-23 08:27:28 -0700

    Initial
    initial

---
+++ Module1/Workflow/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Show a ASCII art of a cat. */
+
+
+
+namespace Workflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("  /\\_/\\");
+            Console.WriteLine(" ( o.o )");
+            Console.WriteLine("  > ^ <");
+            /*escape character \ is used to escape the special character */
+
+        }
+    }
+}
